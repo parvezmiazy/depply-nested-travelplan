de4de3c7dc33453be29113ec21808f201f5863e8
Parvez <miazyparvez@gmail.com>
2024-01-29 12:36:07 +0600

    project completed

--- src/components/TravelPlan.jsx
+++ src/components/TravelPlan.jsx
@@ -5,12 +5,27 @@ export default function TravelPlan() {
   const [plan, setPlan] = useState(initialTravelPlan);
   const root = plan[0];
   const planetIds = root.childIds;
+
+  const handleComplete = (parentId, childId) => {
+    const parent = plan[parentId];
+    const nextParent = {
+      ...parent,
+      childIds: parent.childIds.filter((id) => id !== childId),
+    };
+    setPlan({ ...plan, [parentId]: nextParent });
+  };
   return (
     <>
       <h2>Places to visit</h2>
       <ol>
         {planetIds.map((placeId) => (
-          <PlaceTree key={placeId} id={placeId} placesById={plan} />
+          <PlaceTree
+            key={placeId}
+            id={placeId}
+            placesById={plan}
+            onComplete={handleComplete}
+            parentId={0}
+          />
         ))}
       </ol>
     </>
