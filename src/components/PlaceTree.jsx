3c8df92592882e7d50f65b80939a6acbf75ba835
Parvez <miazyparvez@gmail.com>
2024-01-29 11:59:07 +0600

    depply nested with normalized data

--- src/components/PlaceTree.jsx
+++ src/components/PlaceTree.jsx
@@ -1,13 +1,14 @@
-export default function PlaceTree({ place }) {
-  const childPlaces = place.childPlaces;
+export default function PlaceTree({ id, placesById }) {
+  const place = placesById[id];
+  const childIds = place.childIds;
   return (
     <>
       <li>
         {place.title}
-        {childPlaces.length > 0 && (
+        {childIds.length > 0 && (
           <ol>
-            {childPlaces.map((place) => (
-              <PlaceTree key={place.id} place={place} />
+            {childIds.map((id) => (
+              <PlaceTree key={id} id={id} placesById={placesById} />
             ))}
           </ol>
         )}
--- src/components/TravelPlan.jsx
+++ src/components/TravelPlan.jsx
@@ -1,15 +1,16 @@
 import { useState } from "react";
-import { initialTravelPlan } from "../data/places";
+import { initialTravelPlan } from "../data/places-normalized";
 import PlaceTree from "./PlaceTree";
 export default function TravelPlan() {
   const [plan, setPlan] = useState(initialTravelPlan);
-  const planets = plan.childPlaces;
+  const root = plan[0];
+  const planetIds = root.childIds;
   return (
     <>
       <h2>Places to visit</h2>
       <ol>
-        {planets.map((place) => (
-          <PlaceTree key={place.id} place={place} />
+        {planetIds.map((placeId) => (
+          <PlaceTree key={placeId} id={placeId} placesById={plan} />
         ))}
       </ol>
     </>
